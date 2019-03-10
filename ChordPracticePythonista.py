b80d66982404220832157d64546b3a2ea2543be4
Luke Piszkin <lpiszkin@gmail.com>
2019-03-09 22:10:09 -0800

    Initial commit: open a new view with label and exit button

--- ChordPracticePythonista.py
+++ ChordPracticePythonista.py
@@ -7,6 +7,22 @@ scale = [ '', 'm',  '7',  'maj7', '6', 'm6', '5', '9', 'm9', 'maj9', '11', 'm11'
 randroot = random.randint(0,len(root)-1)
 randscale = random.randint(0,len(scale)-1)
 chord = root[randroot]+scale[randscale]
+minor_txt = 'root, minor third, perfect fifth'
+dom_txt = 'root, major third, pefect fifth, minor seventh, (add 9), (add 9,11), (add 9,11,13)'
+major_7_9_11_13_txt = 'root, major third, perfect fifth, major seventh, (add 9), (add 9,11), (add 9,11,13)'
+add_txt = 'take the root triad and add the desired note in the scale'
+aug_txt = 'root, major third, augmented fifth'
+dim_txt = 'root, minor third, flattened fifth'
+sharp_flat_txt = 'sharp (+,#) or flat (-,b) the desired note and add it to the root triad'
+minor_third_txt = 'An interval consisting of three semitones'
+major_third_txt = 'An interval consisting of four semitones'
+perfect_fourth_txt = 'An interval consisting of five semitones'
+flattened_fifth_txt = 'An interval consisting of six semitones'
+perfect_fifth_txt = 'An interval consisting of seven semitones'
+augmented_fifth_txt = 'An interval consisting of eight semitones'
+sixth_txt = 'An interval consisting of nine semitones'
+minor_seventh_txt = 'An interval consisting of ten semitones'
+major_seventh_txt = 'An interval consisting of eleven semitones'
 
 view=ui.View()
 view.size_to_fit()
@@ -22,16 +38,17 @@ chordtxt.frame = (90,120,200,75)
 chordtxt.alignment = ui.ALIGN_CENTER
 
 def reset(sender):
+
     exec(open("ChordPractice.py").read())
-    
-def quit(sender):
-	view.close()
-	
+
 quitbutton = ui.Button(title = 'QUIT')
 quitbutton.frame = (140,250,100,50)
 quitbutton.border_color = '#0000ff'
 quitbutton.border_width = 10
-quitbutton.action = quit
+quitbutton.action = quit   
+
+def quit(sender):
+	view.close()
 
 resetbutton = ui.Button(title = 'RESET')
 resetbutton.frame = (140, 200, 100, 50)
@@ -39,7 +56,35 @@ resetbutton.border_color = '#0000ff'
 resetbutton.border_width = 10
 resetbutton.action = reset
 
+def minor(sender):
+    view_minor = ui.View()
+    view_minor.size_to_fit()
+    view_minor.background_color = 'white'
+    view_minor.flex = 'WH'
+    minor_lab = ui.Label(text = minor_txt)
+    view_minor.add_subview(minor_lab)
+    def minorquit(sender):
+        view_minor.close()
+    quitbuttonminor = ui.Button(title = 'QUIT')
+    quitbuttonminor.frame = (140,250,100,50)
+    quitbuttonminor.border_color = '#0000ff'
+    quitbuttonminor.border_width = 10
+    quitbuttonminor.action = minorquit
+    view_minor.add_subview(quitbuttonminor)
+
+
+
+
+
+minor = ui.Button(title = 'm (minor)')
+minor.frame = (140, 300, 100, 50)
+minor.border_color = '#0000ff'
+minor.border_width = 5 
+minor.action = minor
+
+
 view.add_subview(chordtxt)
 view.add_subview(resetbutton)
 view.add_subview(quitbutton)
+view.add_subview(minor)
 view.present(hide_title_bar=True)
